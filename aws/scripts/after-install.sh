c5bbb603c851e7baf32077b2e1f49d99b7e21102
Richard Wang <richardrwang@Richards-MacBook-Pro.local>
2024-09-21 21:11:40 -0700

    Changing region.

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-wvyouhcaaerg/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://testcodedeploystack-webappdeploymentbucket-beb7k2j3ear7/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
